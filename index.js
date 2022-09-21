336fbec7523fa89cca1f6c97c0656284ff830eea
SonSonder <maceomaddox@gmail.com>
2022-09-21 19:38:54 -0400

    function is live! next is to append to newREADME.md

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // TODO: Include packages needed for this application
 const fs = require("fs");
 const inquirer = require("inquirer");
-const generateMarkdown = require("utils.generateMarkdown.js");
+const generateMarkdown = require("./utils/generateMarkdown.js");
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -57,6 +57,15 @@ function writeToFile(fileName, data) {
 
 // TODO: Create a function to initialize app
 function init() {
+    inquirer
+        .prompt(questions)
+        .then((answers) => {
+            writeToFile("newREADME.md", answers);
+        })
+        .catch((error) => {
+            console.log(err)
+        });
+};
 
 // Function call to initialize app
 init();
